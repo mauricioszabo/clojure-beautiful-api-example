154bb44bd4c299d83826be00c8880dbcdfb4c86a
Maurício Szabo <mauricio.szabo@gmail.com>
2017-12-03 01:11:48 -0200

    Added project.clj

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject clojure-sugar "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.novemberain/langohr "4.1.0"]]
+  :main ^:skip-aot clojure-sugar.core
+  :target-path "target/%s"
+
+  :profiles {:uberjar {:aot :all}})
